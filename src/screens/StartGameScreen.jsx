85170fb5b051c1b63bcb3bc33374d6c6cd3b76f5
Daniil Borysov <danofu13@gmail.com>
2023-04-04 14:27:20 +0200

    Style NumberInput in StartGameScreen

--- src/screens/StartGameScreen.jsx
+++ src/screens/StartGameScreen.jsx
@@ -5,7 +5,13 @@ import PrimaryButton from 'components/PrimaryButton';
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="number-pad"
+        maxLength={2}
+        style={styles.numberInput}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -26,5 +32,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: '#DDB52F',
+    borderBottomWidth: 2,
+    color: '#DDB52F',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });

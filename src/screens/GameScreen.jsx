ac0d861e422d523688a31a2d17e9936c1d5c9007
Daniil Borysov <danofu13@gmail.com>
2023-04-04 16:54:36 +0200

    Update GameScreen

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -1,7 +1,24 @@
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function GameScreen() {
-  return <Text>Game Screen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Game Screen</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower ?</Text>
+        {/* + -*/}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12
+  }
+});
